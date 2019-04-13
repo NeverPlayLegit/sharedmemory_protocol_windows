922f304dd1240375a2793a28272c3b16209525b8
NeverPlayLegit <34961376+NeverPlayLegit@users.noreply.github.com>
2019-04-13 11:50:44 +0200

    Update sh_client.h

--- sh_client.h
+++ sh_client.h
@@ -5,7 +5,7 @@
 
 typedef struct sh_client_t sh_client_t;
 
-typedef void (*_sh_client_recv_callback)(sh_client_t* sh, int8_t *buf, uint32_t len);
+typedef void (*_sh_client_recv_callback)(sh_client_t* sh, int8_t* buf, uint32_t len);
 
 struct sh_client_t {
 	HANDLE hBuffer_read;
@@ -19,8 +19,8 @@ struct sh_client_t {
 	_sh_client_recv_callback func_callback;
 };
 
-DWORD WINAPI _sh_client_task_recv(void *data) {
-	sh_client_t *sh = (sh_client_t*)data;
+DWORD WINAPI _sh_client_task_recv(void* data) {
+	sh_client_t* sh = (sh_client_t*)data;
 	while (sh->_active) {
 		WaitForSingleObject(sh->hEvent_read2, INFINITE);
 		uint32_t len = sh->buf_read[1] | (uint32_t)sh->buf_read[2] << 8
@@ -39,11 +39,11 @@ Sends buffer to sharedmemory-connection
 
 Blocks until buffer read (read flag ist set)
 */
-void sh_client_send(sh_client_t *sh, int8_t *buf, uint32_t len) {
+void sh_client_send(sh_client_t* sh, int8_t* buf, uint32_t len) {
 	if (sh->_active && len + 5 < sh->buf_len) {
 		WaitForSingleObject(sh->hEvent_write, INFINITE);
 		memcpy(&sh->buf_write[5], buf, len);
-		
+
 		sh->buf_write[4] = (len >> 24) & 0xFF;
 		sh->buf_write[3] = (len >> 16) & 0xFF;
 		sh->buf_write[2] = (len >> 8) & 0xFF;
@@ -51,17 +51,31 @@ void sh_client_send(sh_client_t *sh, int8_t *buf, uint32_t len) {
 
 		sh->buf_write[0] = 1;
 
-		SetEvent(sh->hEvent_write2);		
+		SetEvent(sh->hEvent_write2);
 	}
 }
 
-sh_client_t* sh_client_new(_sh_client_recv_callback callback, const char* shared_name_read, const char* shared_name_write
+void sh_client_close(sh_client_t* sh) {
+	sh->_active = 0;
+	if(sh->hThread_recv)WaitForSingleObject(sh->hThread_recv, INFINITE);
+	if(sh->buf_read)UnmapViewOfFile(sh->buf_read);
+	if (sh->buf_write)UnmapViewOfFile(sh->buf_write);
+	if(sh->hBuffer_read)CloseHandle(sh->hBuffer_read);
+	if(sh->hBuffer_write)CloseHandle(sh->hBuffer_write);
+	if (sh->hEvent_read)CloseHandle(sh->hEvent_read);
+	if (sh->hEvent_read2)CloseHandle(sh->hEvent_read2);
+	if (sh->hEvent_write)CloseHandle(sh->hEvent_write);
+	if (sh->hEvent_write2)CloseHandle(sh->hEvent_write2);
+	free(sh);
+}
+
+sh_client_t * sh_client_new(_sh_client_recv_callback callback, const char* shared_name_read, const char* shared_name_write
 	, const char* event_name_read, const char* event_name_write
 	, const char* event_name_read2, const char* event_name_write2, size_t buf_len) {
 	sh_client_t* res = (sh_client_t*)malloc(sizeof(sh_client_t));
 	res->buf_len = buf_len;
 	res->func_callback = callback;
-	
+
 	res->hBuffer_read = CreateFileMapping(INVALID_HANDLE_VALUE,
 		NULL, PAGE_READWRITE, 0, buf_len, shared_name_read);
 	if (res->hBuffer_read == NULL) {
@@ -77,9 +91,7 @@ sh_client_t* sh_client_new(_sh_client_recv_callback callback, const char* shared
 	res->buf_read = (int8_t*)MapViewOfFile(res->hBuffer_read, FILE_MAP_ALL_ACCESS,
 		0, 0, buf_len);
 	if (res->buf_read == NULL) {
-		CloseHandle(res->hBuffer_read);
-		CloseHandle(res->hBuffer_write);
-		goto err;
+		goto dispose;
 	}
 
 	res->buf_write = (int8_t*)MapViewOfFile(res->hBuffer_write, FILE_MAP_ALL_ACCESS,
@@ -125,16 +137,3 @@ dispose:
 	return 0;
 }
 
-void sh_client_close(sh_client_t *sh) {
-	sh->_active = 0;
-	WaitForSingleObject(sh->hThread_recv, INFINITE);
-	UnmapViewOfFile(sh->buf_read);
-	UnmapViewOfFile(sh->buf_write);
-	CloseHandle(sh->hBuffer_read);
-	CloseHandle(sh->hBuffer_write);
-	CloseHandle(res->hEvent_read);
-	CloseHandle(res->hEvent_read2);
-	CloseHandle(res->hEvent_write);
-	CloseHandle(res->hEvent_write2);
-	free(sh);
-}
